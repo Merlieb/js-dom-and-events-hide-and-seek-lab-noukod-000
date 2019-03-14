b225fc363c45fd3e732705f0de8df847c8606ad8
Merline Belizaire <merline@noukod.com>
2019-03-14 15:54:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function nestedTarget(){
   return document.querySelector('#nested')
 }
 
-function deepestChild(){
-  return document.querySelector('div#grand-node')
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
+function increaseRankBy(n) {
+  let numbers = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].innerHTML = (parseInt(numbers[i].innerHTML) + n).toString()
+  }
 }
